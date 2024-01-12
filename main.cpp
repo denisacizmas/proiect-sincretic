501fa70cdadd545b4d670e356f8e960a965dad83
denisacizmas <cizmasdenisa@gmail.com>
2024-01-12 14:32:45 +0200

    functia de colorare

--- main.cpp
+++ main.cpp
@@ -13,6 +13,31 @@ struct Tara
     string culoare;
 };
 
+void colorare(vector<Tara>& tari, const vector<string>& culori) 
+{
+    srand(static_cast<unsigned int>(time(nullptr)));
+    vector<string> amestecare = culori;
+
+    for (auto& tara : tari) 
+    {
+        set<string> folosita;
+
+        for (const auto& vecin : tara.vecini)
+            for (const auto& altaTara : tari)
+                if (altaTara.nume == vecin && !altaTara.culoare.empty())
+                    folosita.insert(altaTara.culoare);
+
+        for (const auto& culoare : amestecare)
+            if (folosita.count(culoare) == 0) 
+            {
+                tara.culoare = culoare;
+                random_shuffle(amestecare.begin(), amestecare.end());
+                break;
+            }
+    }
+}
+
+
 int main()
 {
 
